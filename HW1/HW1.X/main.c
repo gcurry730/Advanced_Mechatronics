e3794194079d8a88e5abe98f4a18f3e2f367f6d9
gcurry730 <gcurry730@gmail.com>
2016-04-04 23:45:13 -0500

    complied code, first try

--- HW1/HW1.X/main.c
+++ HW1/HW1.X/main.c
@@ -37,5 +37,33 @@
 #pragma config FVBUSONIO = ON // USB BUSON controlled by USB module
 
 int main() {
-	
+	 __builtin_disable_interrupts();
+
+    // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
+    __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
+
+    // 0 data RAM access wait states
+    BMXCONbits.BMXWSDRM = 0x0;
+
+    // enable multi vector interrupts
+    INTCONbits.MVEC = 0x1;
+
+    // disable JTAG to get pins back
+    DDPCONbits.JTAGEN = 0;
+    
+   TRISAbits.TRISA4 = 0;        // Pin A4  is the LED, 0 for output
+   LATAbits.LATA4 = 1;          // make LED pin "low" (0) = "on"
+    
+    __builtin_enable_interrupts();
+    
+    while(1) {
+	    // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
+		// remember the core timer runs at half the CPU speed
+        _CP0_SET_COUNT(0);
+        if(_CP0_GET_COUNT()==40000 ){   //40 M ticks per second so ...
+            _CP0_SET_COUNT(0);
+            LATAINV=0x10; 
+        }
+        
+    }
 }
