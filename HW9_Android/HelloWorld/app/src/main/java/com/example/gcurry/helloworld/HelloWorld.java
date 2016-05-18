86066bfb6bb2817b2bb48c87ba3a895ddad2c71c
gcurry730 <gcurry730@gmail.com>
2016-05-17 22:39:23 -0500

    Hello World for Android completed

---
+++ HW9_Android/HelloWorld/app/src/main/java/com/example/gcurry/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.gcurry.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloWorld extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_world);
+    }
+}
