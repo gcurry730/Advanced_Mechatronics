ec9c0277234e996964d157c18d7e0ec01eafa1bb
gcurry730 <gcurry730@gmail.com>
2016-05-24 12:32:53 -0500

    Started HW13

--- HW9_Android/HelloWorld/app/src/main/java/com/example/gcurry/helloworld/HelloWorld.java
+++ HW9_Android/HelloWorld/app/src/main/java/com/example/gcurry/helloworld/HelloWorld.java
@@ -2,12 +2,47 @@ package com.example.gcurry.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 
 public class HelloWorld extends AppCompatActivity {
+    SeekBar myControl;
+    TextView myTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_world);
+        myControl = (SeekBar) findViewById(R.id.seek1);
+        myTextView = (TextView) findViewById(R.id.textView01);
+        setMyControlListener();
     }
-}
+    private void setMyControlListener() {
+        myControl.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+            int progressChanged = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
+                myTextView.setText("The value is: "+progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+
+        });
+
+    }
+};
+
+
+
+
